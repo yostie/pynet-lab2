200100cc70a0910c7fa7e57261818c8dcbb9b613
yostie <yostie6909@gmail.com>
2015-01-28 15:03:01 -0500

    Week 2 Exercises
    Week 2 Exercises

---
+++ ex1.py
@@ -0,0 +1,138 @@
+'''
+Author: Mike Yost
+
+Version: 1.0
+
+Purpose: Determine whether or not the running configuration has changed during a specific time interval.  If it has, send an alert email.  Check status every 5 minutes.
+
+'''
+
+# IMPORT LIBRARIES
+
+import snmp_helper
+import email_helper
+import os.path
+import time
+import pickle
+
+# CONSTANTS
+
+IP = '50.242.94.227'
+SNMP_PORT = 8061
+SNMP_CREDS = {
+	'user' : 'pysnmp',
+	'auth_key' : 'galileo1',
+	'encr_key' : 'galileo1'
+}
+OIDS = {
+	'run_last_changed' : '1.3.6.1.4.1.9.9.43.1.1.1.0',
+	'start_last_changed' : '1.3.6.1.4.1.9.9.43.1.1.3.0',
+	'sysname' : '1.3.6.1.2.1.1.5.0'
+}
+
+SLEEP_INTERVAL = 300
+
+# DEFINE FUNCTIONS
+
+def init_run_last_changed():
+
+	if os.path.exists("run_last_changed.pkl"):
+
+		print "Save file exists.  Loading save file...\n"
+		file = open("run_last_changed.pkl", "rb")
+		file_data = pickle.load(file)
+		file.close()
+		return int(file_data)
+
+	else:
+
+		print "First time script has been run.  Initializing save file...\n"
+		file = open("run_last_changed.pkl", "wb")
+		file_data = '0'
+		pickle.dump(file_data, file)
+		file.close()
+		return int(file_data)
+
+def dump_run_last_changed(file_data):
+	file = open ("run_last_changed.pkl", "wb")
+	pickle.dump(file_data, file)
+	file.close()
+	return True
+		
+def check_run_changed(previous_run_change_time, current_run_change_time):
+	
+	if current_run_change_time > previous_run_change_time:
+		return True
+	else:
+		return False
+
+def send_alert_mail(dev_name, run_last_changed):
+
+	recipient = 'mikey@fltg.com'
+	subject = "Running config of %s was changed at %s" % (dev_name, run_last_changed)
+	message = '''You better look at this.
+'''
+	sender = 'pynetlab@twb-tech.com'
+	email_helper.send_mail(recipient, subject, message, sender)
+	
+	return True
+
+def snmp_poll(dev, user, oid):
+
+	snmp_raw = snmp_helper.snmp_get_oid_v3(dev, user, oid)
+	
+	return snmp_helper.snmp_extract(snmp_raw)
+
+def main():
+
+	debug = False
+
+	device = (IP, SNMP_PORT)
+	snmp_user = (SNMP_CREDS.get('user'), SNMP_CREDS.get('auth_key'), SNMP_CREDS.get('encr_key'))
+	iterations = 12
+	previous_run_last_changed = init_run_last_changed()
+	sysname = snmp_poll(device, snmp_user, oid=OIDS.get('sysname'))
+	
+	if previous_run_last_changed == 0:
+		
+		print "Polling router for the last time the running config was changed..."
+		
+		current_run_last_changed = int(snmp_poll(device, snmp_user, oid=OIDS.get('run_last_changed')))
+		dump_run_last_changed(current_run_last_changed)
+		previous_run_last_changed = current_run_last_changed
+		
+		if debug is True:
+			print "Previous: %s" % previous_run_last_changed
+			print "Current: %s" % current_run_last_changed
+			print "Iteration: %s\n" % iterations
+		
+		iterations = iterations - 1	
+		time.sleep(SLEEP_INTERVAL)
+
+	while iterations != 0:
+
+		print "Polling router for the last time the running config was changed..."
+		
+		current_run_last_changed = int(snmp_poll(device, snmp_user, oid=OIDS.get('run_last_changed')))
+		dump_run_last_changed(current_run_last_changed)
+		run_changed_status = check_run_changed(previous_run_last_changed, current_run_last_changed)
+		previous_run_last_changed = current_run_last_changed
+
+		if run_changed_status is True:
+		
+			print "The running config has changed in this 300 second interval.  Sending email notification."
+			send_alert_mail(sysname, current_run_last_changed)
+
+		if debug is True:
+			print "Previous: %s" % previous_run_last_changed
+			print "Current: %s" % current_run_last_changed
+			print "Iteration: %s\n" % iterations
+		
+		iterations = iterations - 1
+		time.sleep(SLEEP_INTERVAL)
+
+	print "The script has finished running."
+
+if __name__ == '__main__':
+
+    main()
---
+++ ex2.py
@@ -0,0 +1,113 @@
+'''
+Author: Mike Yost
+
+Version: 1.0
+
+Purpose: Poll router interface stats every SLEEP_INTERVAL for a MAX_RUNTIME then print a line chart of the data
+
+'''
+
+# IMPORT LIBRARIES
+
+import snmp_helper
+import pygal
+import time
+
+# CONSTANTS
+
+IP = '50.242.94.227'
+SNMP_PORT = 7961
+SNMP_CREDS = {
+	'user' : 'pysnmp',
+	'auth_key' : 'galileo1',
+	'encr_key' : 'galileo1'
+}
+OIDS = {
+	'ifDescr_fa4' : '1.3.6.1.2.1.2.2.1.2.5',
+	'ifInOctets_fa4' : '1.3.6.1.2.1.2.2.1.10.5',
+	'ifInUcastPkts_fa4' : '1.3.6.1.2.1.2.2.1.11.5',
+	'ifOutOctets_fa4' : '1.3.6.1.2.1.2.2.1.16.5',
+	'ifOutUcastPkts_fa4' : '1.3.6.1.2.1.2.2.1.17.5'
+}
+
+SLEEP_INTERVAL = 300
+MAX_RUNTIME = 3600
+
+# DEFINE FUNCTIONS
+
+def snmp_poll(dev, user, oid):
+
+	''' 
+	PURPOSE: Perform an SNMPv3 poll of a device.
+	INPUT: Takes a 'device' object containing IP and SNMP_PORT, 'user' object containing the SNMPv3 credentials, and the 'oid' to be polled.
+	OUTPUT: Returns a readable form of the SNMP data.
+	'''
+	
+	snmp_raw = snmp_helper.snmp_get_oid_v3(dev, user, oid)
+	
+	return snmp_helper.snmp_extract(snmp_raw)
+
+def create_graph(title, labels, in_desc, in_val, out_desc, out_val, filename):
+
+	'''
+	PURPOSE: Creates an input/output line chart.
+	INPUT: Chart title, Chart X-axis labels, input data description, input, data, output data description, output data, and filename for chart to be created.
+	OUTPUT: Returns nothing but does create a chart.
+	'''
+
+	print 'Creating graph %s...' % title
+
+	line_chart = pygal.Line()
+	line_chart.title = title
+	line_chart.x_labels = labels
+	line_chart.add(in_desc, in_val)
+	line_chart.add(out_desc, out_val)
+	line_chart.render_to_file(filename)
+
+def main():
+
+	debug = False
+
+	# INITIALIZE VARIABLES
+	
+	device = (IP, SNMP_PORT)
+	snmp_user = (SNMP_CREDS.get('user'), SNMP_CREDS.get('auth_key'), SNMP_CREDS.get('encr_key'))
+	
+	current_sleeptime = 0
+	
+	in_octets = []
+	out_octets = []
+	in_ucast_pkts = []
+	out_ucast_pkts = []
+	
+	chart_labels = []
+	
+	while current_sleeptime <= MAX_RUNTIME:
+		
+		'''
+		As long as the current_sleeptime is less than or equal to the MAX_RUNTIME,
+		then perform an SNMP poll for interface statistics.
+		
+		Add the SLEEP_TIME as a chart X-axis label then go to sleep for SLEEP_INTERVAL.
+		'''
+		
+		print "Grabbing interface statistics for interval %s ..." % current_sleeptime
+		
+		in_octets.append(int(snmp_poll(device, snmp_user, oid=OIDS.get('ifInOctets_fa4'))))
+		out_octets.append(int(snmp_poll(device, snmp_user, oid=OIDS.get('ifOutOctets_fa4'))))
+		in_ucast_pkts.append(int(snmp_poll(device, snmp_user, oid=OIDS.get('ifInUcastPkts_fa4'))))
+		out_ucast_pkts.append(int(snmp_poll(device, snmp_user, oid=OIDS.get('ifOutUcastPkts_fa4'))))
+
+		chart_labels.append(str(current_sleeptime))
+		current_sleeptime = current_sleeptime + SLEEP_INTERVAL
+		time.sleep(SLEEP_INTERVAL)
+	
+	# CREATE GRAPHS OF INTERFACE DATA
+	
+	print "All data has been collected.  Beginning to create graphs of data."
+	create_graph('Input/Output Octets', chart_labels, 'InOctets', in_octets, 'OutOctets', out_octets, 'octets.svg')
+	create_graph('Input/Output Unicast Packets', chart_labels, 'InPackets', in_ucast_pkts, 'OutPackets', out_ucast_pkts, 'packets.svg')
+
+if __name__ == '__main__':
+
+    main()
